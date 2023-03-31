f132a9a4e073b9d931d2d10eb51e98dcb07d4dc1
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-31 12:54:35 -0300

    feat: adding interval component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+
 import Card from './components/Card';
+import Intervalo from './components/Intervalo';
 
 function App() {
   return (
     <div className="App">
       <h1>Exercicio react-redux (simples)</h1>
       <div className='linha'>
-        <Card title="Card 1" red>x</Card>
-        <Card title="Card 2" green>x</Card>
+        <Intervalo title="Card 1" red>x</Intervalo>
       </div>
       <div className='linha'>
+        <Card title="Card 2" green>x</Card>
         <Card title="Card 3" blue>y</Card>
         <Card title="Card 4" purple>y</Card>
       </div>
---
+++ src/components/Intervalo.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-anonymous-default-export */
+import "./Intervalo.css";
+import React from "react";
+import Card from "./Card";
+
+export default (props) => {
+  return (
+    <Card title="Intervalo de numeros" red>
+      <div className="Intervalo">
+        <span>
+          <strong>Minimo:</strong>
+          <input type="number" value={0} readOnly />
+        </span>
+        <span>
+          <strong>Maximo:</strong>
+          <input type="number" value={0} readOnly />
+        </span>
+      </div>
+    </Card>
+  );
+};
