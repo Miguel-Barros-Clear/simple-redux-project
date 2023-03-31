3c72b195f7fb3cdcdab88adf6950f994902204e4
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-31 12:49:38 -0300

    feat: adding Card component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ function App() {
     <div className="App">
       <h1>Exercicio react-redux (simples)</h1>
       <div className='linha'>
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Card 1" red>x</Card>
+        <Card title="Card 2" green>x</Card>
       </div>
       <div className='linha'>
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Card 3" blue>y</Card>
+        <Card title="Card 4" purple>y</Card>
       </div>
     </div>
   );
