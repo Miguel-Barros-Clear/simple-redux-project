66ee4b611259311e75a24aca3e509799bcd283d2
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-31 13:12:37 -0300

    feat: adding application not REDUX

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 
-import Card from './components/Card';
 import Intervalo from './components/Intervalo';
 import Soma from './components/Soma';
 import Media from './components/Media';
 import Sorteio from './components/Sorteio';
 
 function App() {
+
+  const [min, setMin] = useState(10)
+  const [max, setMax] = useState(20)
+
   return (
     <div className="App">
       <h1>Exercicio react-redux (simples)</h1>
       <div className='linha'>
-        <Intervalo title="Card 1" red>x</Intervalo>
+        <Intervalo title="Card 1" min={min} max={max} red onMinChanged={setMin} onMaxChanged={setMax}></Intervalo>
       </div>
       <div className='linha'>
-        <Media></Media>
-        <Soma></Soma>
-        <Sorteio></Sorteio>
+        <Media min={min} max={max}></Media>
+        <Soma min={min} max={max}></Soma>
+        <Sorteio min={min} max={max}></Sorteio>
       </div>
     </div>
   );
