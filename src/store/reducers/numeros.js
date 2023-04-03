dcf40d6a85a9dea8f5b334d6a2e29d37c462aa27
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-03 08:54:33 -0300

    perf: adding constant values for reducers

--- src/store/reducers/numeros.js
+++ src/store/reducers/numeros.js
@@ -1,3 +1,8 @@
+import {
+    NUM_MIN_ALTERADO,
+    NUM_MAX_ALTERADO
+} from '../actions/actionsTypes'
+
 const initialState = {
     min: 1,
     max: 10
@@ -5,13 +10,13 @@ const initialState = {
 
 export default function (state = initialState, action) {
     switch (action.type) {
-        case 'NUM_MIN_ALTERADO':
+        case NUM_MIN_ALTERADO:
             return {
                 ...state,
                 min: action.payload
             }
 
-        case 'NUM_MAX_ALTERADO':
+        case NUM_MAX_ALTERADO:
             return {
                 ...state,
                 max: action.payload
